c7d71c9851bd9147c8ab9dac7dcafb3899d02f2b
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-10-19 01:25:32 +0200

    Update tests

--- test/jestsetup.js
+++ test/jestsetup.js
@@ -1,3 +1,4 @@
+/* global global */
 // Make Enzyme functions available in all test files without importing
 import { shallow, render, mount } from 'enzyme';
 
@@ -5,6 +6,7 @@ global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 // Fail tests on any warning
+// eslint-disable-next-line
 console.error = message => {
   throw new Error(message);
 };
