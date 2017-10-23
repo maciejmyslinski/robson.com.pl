2b6213721d52b9090da86dc41094cb75a8c3d225
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-10-23 04:08:53 +0200

    Fix storybook

--- src/components/MainNavbar/index.stories.js
+++ src/components/MainNavbar/index.stories.js
@@ -3,11 +3,19 @@ import { storiesOf } from '@storybook/react';
 import { MemoryRouter } from 'react-router';
 import { storybookDefaults } from 'hocs/storybook';
 import MainNavbar from './';
+import logo from 'images/logo.png';
 
 storiesOf('MainNavbar', module).add('default', () => {
+  const logoData = {
+    resolutions: {
+      srcSet: `${logo} 1x`,
+      src: logo,
+      width: 198,
+    },
+  };
   const Component = () => (
     <MemoryRouter>
-      <MainNavbar env="test" />
+      <MainNavbar env="test" logo={logoData} />
     </MemoryRouter>
   );
   return storybookDefaults(Component);
