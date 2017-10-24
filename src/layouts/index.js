3b5117a5241f65c12540c9c4f63acae6c00afb8f
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-10-25 01:33:08 +0200

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -36,7 +36,29 @@ const TemplateWrapper = ({ data, children }) => (
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
-    />
+    >
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+      <meta name="theme-color" content="#424242" />
+    </Helmet>
     <MainNavbar logo={data.logo} />
     {children()}
     <link
