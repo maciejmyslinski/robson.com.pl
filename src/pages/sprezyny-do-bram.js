bc601a083815c5bd404c9259cbb4ef32da1e4993
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-12-24 23:57:03 +0100

    add springs page content

--- src/pages/sprezyny-do-bram.js
+++ src/pages/sprezyny-do-bram.js
@@ -1,5 +1,55 @@
 import React from 'react';
 
-const SpringsPage = () => <h1>Hello from sprężyny</h1>;
+const SpringsPage = () => (
+  <div>
+    <h1>Sprężyny do bram</h1>
+    <h2>Jakość</h2>
+    <p>
+      Sprężyny, które dostarczamy są poddane obróbce kulowania. Dzięki temu są
+      one znacznie bardziej wytrzymałe.
+    </p>
+    <p>
+      <b>Czym jest kulowanie?</b> Najprościej mówiąc, kulowanie polega na
+      bombardowaniu materiału tysiącami maleńkich drobinek. Powierzchnia tak
+      obrobionego kawałeka metalu zmienia swoją strukturę i geometrię. Powstaje
+      w niej cała siatka tak zwanych naprężeń ściskających.{' '}
+      <b>Trwałość sprężyny skrętnej wzrasta o 100%!</b> Ponadto sprężyna
+      wykonana z takiego drutu jest znacznie bardziej{' '}
+      <b>odporna na korozję oraz powstawanie mikropęknięć.</b>
+    </p>
+    <p>
+      Technologię kulowania wykorzystuje się między innymi do kształtowania
+      aerodynamicznych krzywizn metalowych powierzchni skrzydeł w przemyśle
+      lotniczym. Teraz technologia wysokich lotów również w twoim garażu!
+    </p>
+    <h2>Szybkość</h2>
+    <p>
+      Pęknięta sprężyna często nie pozwala na otwarcie bramy. Rozumiemy jak
+      kłopotliwe może się to okazać, dlatego wychodzimy naprzeciw potrzebom
+      naszych klientów i dostarczamy sprężyny do bram tak szybko, jak to tylko
+      możliwe.
+    </p>
+    <p>
+      Dział naszej firmy specjalizujący się w sprężynach do bram garażowych oraz
+      przemysłowych jest gotowy na przygotowanie sprężyny tuż po tym, jak
+      złożysz zamówienie. Sprężynę otrzymasz w ciągu jednego dnia roboczego.
+    </p>
+    <p>
+      <b>
+        W obrębie Poznania jest możliwość wymiany sprężyn również w weekend.
+      </b>{' '}
+      Nie zwlekaj!
+    </p>
+    <h2>Przystępne ceny</h2>
+    <p>
+      Dysponujemy sprężynami oryginalnymi jak i zamiennikami. Dzięki temu możesz
+      zdecydować się na sprężynę w różnym przedziale cenowym.
+    </p>
+    <h2>Miła obsługa</h2>
+    <p>Zapewniamy również miłą obsługę!</p>
+    <h2>Zadowolenie</h2>
+    <p>Nasze motto: zadowolony klient</p>
+  </div>
+);
 
 export default SpringsPage;
