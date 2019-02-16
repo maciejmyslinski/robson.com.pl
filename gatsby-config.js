0369ea6b6a1f443c56edcc8cfe327dcb3f8cbeaf
Maciej Myśliński <maciej.myslinski@netguru.co>
2019-02-16 23:07:25 +0100

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,18 +10,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-favicon`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        logo: `images/favicon.png`,
       },
     },
-    `gatsby-plugin-styled-components`,
   ],
-};
+}
