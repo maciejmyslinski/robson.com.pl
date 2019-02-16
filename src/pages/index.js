4bbd6b62325fac1d59e1a10d56b803b86eddcff0
Maciej Myśliński <maciej.myslinski@netguru.co>
2019-02-16 20:47:18 +0100

    Configuring absolute imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
-import Layout from '../components/layout';
-import Image from '../components/image';
-import SEO from '../components/seo';
+import Layout from 'components/layout';
+import Image from 'components/image';
+import SEO from 'components/seo';
 
 const H1 = styled.h1`
   color: red;
