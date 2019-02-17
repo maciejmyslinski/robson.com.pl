c04cc4e96488f37c62bfa51941fd9667e6c140f1
Maciej Myśliński <maciej.myslinski@netguru.co>
2019-02-17 22:09:19 +0100

    add sensible global styles

--- src/components/root.js
+++ src/components/root.js
@@ -3,7 +3,19 @@ import PropTypes from 'prop-types'
 import reset from 'styled-reset'
 import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`${reset}`
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body, html {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    box-sizing: border-box;
+    overflow-x: hidden;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`
 
 export const Root = ({ children }) => (
   <>
